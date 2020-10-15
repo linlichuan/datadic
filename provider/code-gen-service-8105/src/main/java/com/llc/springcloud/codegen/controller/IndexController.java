dc88c9cfc8d90fa4d823e00f0bc3a4983458d711
wwdxllc007 <wwdxllc007>
2020-10-15 17:05:41 +0800

    xml转json

--- provider/code-gen-service-8105/src/main/java/com/llc/springcloud/codegen/controller/IndexController.java
+++ provider/code-gen-service-8105/src/main/java/com/llc/springcloud/codegen/controller/IndexController.java
@@ -1,6 +1,9 @@
 package com.llc.springcloud.codegen.controller;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.llc.springcloud.codegen.xml.MybatisGenConfigParser;
+import com.llc.springcloud.util.response.JsonResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +13,10 @@ import javax.annotation.Resource;
 @RestController
 public class IndexController {
 	
-	@Resource
-	private MybatisGenConfigParser mybatisGenConfigParser;
 	
 	@RequestMapping(value = "index", method = RequestMethod.GET)
-	public void index() {
-		mybatisGenConfigParser.parse("pc_GeneratorConfig.xml");
+	public JsonResponse<JSONObject> index() {
+		return JsonResponse.ok(MybatisGenConfigParser.getInstance().parse(null));
 	}
 	
 }
--- provider/code-gen-service-8105/src/main/java/com/llc/springcloud/codegen/xml/MybatisGenConfigParser.java
+++ provider/code-gen-service-8105/src/main/java/com/llc/springcloud/codegen/xml/MybatisGenConfigParser.java
@@ -1,42 +1,96 @@
 package com.llc.springcloud.codegen.xml;
 
+import com.alibaba.fastjson.JSONObject;
+import com.llc.springcloud.util.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
+import javax.xml.stream.XMLEventFactory;
+import javax.xml.stream.XMLEventWriter;
 import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.URL;
 
-@Component
 public class MybatisGenConfigParser {
 	
+	public static final String DEFAULT_CONFIG_TMP_PATH = "gen-config.xml";
 	private final static Logger log = LoggerFactory.getLogger(MybatisGenConfigParser.class);
+	private InputStream in;
+	private static MybatisGenConfigParser parser;
+	private boolean hasResource = false;
+	private String path;
+	private XMLStreamReader reader;
 	
-	public void parse(String path) {
+	private MybatisGenConfigParser() {
+		super();
+	}
+	
+	public JSONObject parse(String path) {
+		if (StringUtil.isBlank(path)) {
+			this.path = DEFAULT_CONFIG_TMP_PATH;
+			getResource(false);
+		} else {
+			this.path = path;
+			getResource(true);
+		}
+		JSONObject result = new JSONObject();
 		XMLInputFactory xmlInputFactory = XMLInputFactory.newFactory();
-		InputStream in;
 		try {
-			URL url = MybatisGenConfigParser.class.getClassLoader().getResource(path);
-			if (url == null) {
-				return;
+			if (!hasResource || in == null) {
+				return result;
 			}
-			in = url.openStream();
-			XMLStreamReader xmlReader = xmlInputFactory.createXMLStreamReader(in);
+			reader = xmlInputFactory.createXMLStreamReader(in);
+			getXmlElement(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (!result.isEmpty()) {
+			log.info(result.toJSONString());
+		}
+		return result;
+	}
+	
+	public static void writeXml() {
+		try {
+//			String tmpFile = FileUtil.copyFileToTmpDir(in);
+			XMLOutputFactory xmlOutputFactory = XMLOutputFactory.newFactory();
+			XMLEventWriter writer = xmlOutputFactory.createXMLEventWriter(new FileOutputStream("F:\\1.xml"));
+			XMLEventFactory xmlEventFactory = XMLEventFactory.newFactory();
+			writer.add(xmlEventFactory.createStartDocument());
+			writer.add(xmlEventFactory.createStartElement("", "namespaceUri", "localName"));
+			writer.add(xmlEventFactory.createAttribute("localName", "value"));
+			writer.add(xmlEventFactory.createEndElement("", "namespaceUri", "localName"));
+			writer.add(xmlEventFactory.createEndDocument());
+			writer.flush();
+		} catch (XMLStreamException | FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void getXmlElement(JSONObject object) {
+		String element = "";
+		try {
 			int type;
-			while (xmlReader.hasNext()) {
-				type = xmlReader.next();
-				log.info("type is {}", type);
-				switch (type) {
-					case XMLStreamConstants.START_ELEMENT:
-					case XMLStreamConstants.END_ELEMENT:
-						log.info(xmlReader.getName().toString());
-						break;
-					case XMLStreamConstants.CHARACTERS:
-						log.info(xmlReader.getText());
-						break;
+			while (reader.hasNext()) {
+				type = reader.next();
+				if (type == XMLStreamConstants.START_ELEMENT) {
+					JSONObject json = new JSONObject();
+					element = reader.getName().getLocalPart();
+					int count = reader.getAttributeCount();
+					if (count == 0) {
+						continue;
+					}
+					for (int i = 0; i < count; i++) {
+						json.put(reader.getAttributeName(i).getLocalPart(), reader.getAttributeValue(i));
+					}
+					object.put(element, json);
 				}
 			}
 		} catch (Exception e) {
@@ -44,5 +98,39 @@ public class MybatisGenConfigParser {
 		}
 	}
 	
+	public void getResource(boolean isAbsolutePath) {
+		try {
+			if (isAbsolutePath) {
+				if (StringUtil.isBlank(path)) {
+					hasResource = false;
+				} else {
+					in = new FileInputStream(path);
+					hasResource = true;
+				}
+			} else {
+				if(StringUtil.isBlank(path)) {
+					path = DEFAULT_CONFIG_TMP_PATH;
+				}
+				URL url = MybatisGenConfigParser.class.getClassLoader().getResource(path);
+				if (url != null) {
+					in = url.openStream();
+					hasResource = true;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			hasResource = false;
+		}
+	}
 	
+	public static MybatisGenConfigParser getInstance() {
+		if (parser == null) {
+			synchronized (MybatisGenConfigParser.class) {
+				if (parser == null) {
+					parser = new MybatisGenConfigParser();
+				}
+			}
+		}
+		return parser;
+	}
 }
